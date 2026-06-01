f560bab38982a48c6e1a0322c0da149fc8973155
SprintXXL <mspmain@pm.me>
2026-06-01 19:49:24 +0200

    Primitive Materials Base Mod

---
+++ src/main/java/com/SprintXXL/primitivematerials/PrimitiveMaterials.java
@@ -0,0 +1,24 @@
+package com.SprintXXL.primitivematerials;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class PrimitiveMaterials
+{
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}
